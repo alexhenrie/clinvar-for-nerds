23e49d12cf5643819e3a213218b6c703918d2482
Alex Henrie <alex.henrie@utah.edu>
2016-02-21 23:06:47 -0700

    Increase memory allowance of database import script to 16 GiB

--- setup.sh
+++ setup.sh
@@ -3,6 +3,6 @@
 ./setup/npm-install.sh
 ./setup/wget-deps.sh
 ./setup/generate-schema.js
-./setup/import-database.js
+./setup/import-database.sh
 ./setup/generate-examples.js
 ./setup/create-indexes.js
